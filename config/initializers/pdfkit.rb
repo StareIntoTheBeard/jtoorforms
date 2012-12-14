c27cb5d85a6ec19a94ac66afe171fbc419299c56
justinwup <justin@winduppixel.com>
2012-12-14 12:57:31 -0600

    heroku config for pdf gen

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,8 +1,7 @@
 # config/initializers/pdfkit.rb
 PDFKit.configure do |config|
      config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if Rails.env.production?  
- end
- 
+     
   config.default_options = {
       :ignore_load_errors => true
     }

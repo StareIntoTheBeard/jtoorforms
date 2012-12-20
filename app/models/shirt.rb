b50267f736130da7fadce0823d62c2096fba6f6c
Justin Bean <justin@winduppixel.com>
2012-12-19 22:26:59 -0600

    added additional photo fields to model

--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -1,5 +1,7 @@
 class Shirt < ActiveRecord::Base
-  attr_accessible :phone_number, :customer_name, :photo_front, :photo_side, :photo_back
+  attr_accessible :phone_number, :customer_name, :photo_front, :photo_side, :photo_back, :address, :city, :state, :zip, :email, :measure_neck, 
+    :measure_overarm, :measure_chest, :measure_stomach, :measure_shoulder, :measure_wrist, :measure_bicep, :measure_length,
+    :measure_sleeve, :measure_hips
   default_scope :order => 'created_at ASC'
   has_attached_file :photo_front,
   	:storage => :dropbox,
@@ -7,18 +9,16 @@ class Shirt < ActiveRecord::Base
     :dropbox_options => {
     	:path => proc { |style| "#{id}/#{photo_front.original_filename}" }
     }
-  # has_attached_file :photo_side,
- 	# :storage => :dropbox,
-  #   :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
-  #   :styles => { :medium => '300x300'},
-  #   :dropbox_options => {
-  #   	:path => "#{shirt.id}/photo_front"
-  #   }
-  # has_attached_file :photo_back,
-  # 	:storage => :dropbox,
-  #   :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
-  #   :styles => { :medium => '300x300'},
-  #   :dropbox_options => {
-  #   	:path => "#{shirt.id}/photo_front"
-  #   }
+  has_attached_file :photo_side,
+ 	:storage => :dropbox,
+    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
+    :dropbox_options => {
+    	:path => proc { |style| "#{id}/#{photo_side.original_filename}" }
+    }
+  has_attached_file :photo_back,
+  	:storage => :dropbox,
+    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
+    :dropbox_options => {
+    	:path => proc { |style| "#{id}/#{photo_back.original_filename}" }
+    }
 end

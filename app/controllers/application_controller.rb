834c7d72160d4b59f3945efd5e6df211d8c4d3cd
Justin Bean <justin@winduppixel.com>
2012-12-22 12:16:48 -0600

    added some additional text and formatting to photo display helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 require 'dropbox_sdk'
 
-# This is an example of a Rails 3 controller that authorizes an application
-# and then uploads a file to the user's Dropbox.
-
-# Test env values here:
-# APP_KEY = "lpzgnc3v39u2527"
-# APP_SECRET = "a0pj795b0gy1fi4"
-# ACCESS_TYPE = :app_folder
-#prod below
 APP_KEY = DROPBOX_CONFIG['app_key']
 APP_SECRET = DROPBOX_CONFIG['app_secret']
 ACCESS_TYPE = :dropbox 
